d7da9920ad02ee6ad07e1a75135df89e53609521
Thiago Andrade Silva <thiago.adsix@gmail.com>
2023-08-10 00:34:34 -0300

    feat: create a dog package

---
+++ dog.go
@@ -0,0 +1,9 @@
+package dog
+
+import (
+	"strings"
+)
+
+func WhenGrouUp(input string) string {
+	return "When the puppy group it says: " + strings.ToUpper(input)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/thiagoadsix/dog
+
+go 1.20
